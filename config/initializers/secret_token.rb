34a9c2510da0240543e09f88809f1ba126608cad
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-10-06 01:03:23 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capricornus::Application.config.secret_key_base = 'f95f56a03cc7f3bb7044505aa07a79299f6593318ff041effa6f21fad766191969fa6f2fe84e498fa37e14415d27dbd31ceea0e94019f842950e44687ce2c9b0'
