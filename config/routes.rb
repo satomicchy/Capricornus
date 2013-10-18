7ce455a6c46418580dc33f63ea91722cd6537338
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-10-18 19:40:14 +0900

    setting root url

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Capricornus::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'journals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
