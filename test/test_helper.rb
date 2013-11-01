82063b493103bc51a8590fc630e3350d8c5863c1
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-11-01 19:58:36 +0900

    set devise login for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,11 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+  setup do
+    @user = users(:kuma)
+    sign_in @user
+  end
+end
