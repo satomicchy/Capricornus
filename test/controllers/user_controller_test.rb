ebb69b4f785cf383d1301b151ad7a6a93607f4f2
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-11-05 18:22:58 +0900

    implement users/new

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -10,4 +10,9 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:users)
   end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 end
