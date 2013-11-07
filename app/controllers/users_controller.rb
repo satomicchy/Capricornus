ee3d862119d5bde000783afe84ada105d040ccca
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-11-07 19:37:05 +0900

    implement users/create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,10 +1,7 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  setup do
-    @user = users(:kuma)
-  end
-
   test "should get index" do
     get :index
     assert_response :success
@@ -14,5 +11,13 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, user: { name: "うさ", email: "usa@example.com", password: "usa_password", password_confirmation: "usa_password" }
+    end
+
+    assert_redirected_to users_path
   end
 end
