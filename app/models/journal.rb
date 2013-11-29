19c80111a7232b4dc443eb10f9e099791eb9b63a
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-11-29 20:44:48 +0900

    implement journal#connect_to_invoice

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -5,9 +5,23 @@ class Journal < ActiveRecord::Base
   belongs_to :estimate
   belongs_to :custom
 
+  after_create :connection_to_invoice
+
   def self.current_month(date)
     first_time = date.beginning_of_month.to_time
     end_time   = first_time.end_of_month
     Journal.where(start_at: first_time..end_time)
+  end
+
+  def connection_to_invoice
+    invoice = Invoice.where(custom_id: custom_id).where("? <= ask_on", start_at.to_date).first
+    if invoice.present?
+      update_attributes(invoice_id: invoice.id)
+    else
+      ask_on      = Date.new(start_at.year, start_at.month, Company.all.first.close_day)
+      new_invoice = Invoice.new(ask_on: ask_on, deadline: ask_on.end_of_month, custom_id: custom_id)
+      new_invoice.save
+      update_attributes(invoice_id: new_invoice.id)
+    end
   end
 end
--- test/models/journal_test.rb
+++ test/models/journal_test.rb
@@ -5,4 +5,21 @@ class JournalTest < ActiveSupport::TestCase
     assert Journal.current_month(Date.today).include?(journals(:today))
     assert_equal 1 , Journal.current_month(Date.today).count
   end
+
+  test "should connect to new invoice" do
+    custom = customs(:fish_shop)
+    today_beginning = DateTime.now.beginning_of_day
+    org_invoice_count = Invoice.count
+    Journal.create(start_at: today_beginning, finish_at: today_beginning + 8.hours, custom_id: custom.id)
+    assert_equal org_invoice_count + 1, Invoice.count
+  end
+
+  test "should connect to present invoice" do
+    custom = customs(:fish_shop)
+    beginning_time = DateTime.new(2013,10,20)
+    org_invoice_count = Invoice.count
+    journal = Journal.create(start_at: beginning_time, finish_at: beginning_time + 8.hours, custom_id: custom.id)
+    assert_equal org_invoice_count, Invoice.count
+    assert_equal custom.id, journal.custom_id
+  end
 end
