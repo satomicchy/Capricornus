72b86d0a035b77f7574ce6fa30d23e01097a37fb
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-12-01 19:19:15 +0900

    hide edit journal and cost buttons when invoice.payment == true

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -8,4 +8,8 @@ module JournalsHelper
     params[:month] ?
       Date.new(params[:year].to_i, params[:month].to_i, 1) - 1.month : Date.today - 1.month
   end
+
+  def edit_ok?
+    @journal.invoice_id.present? && Invoice.find(@journal.invoice_id).payment == true
+  end
 end
