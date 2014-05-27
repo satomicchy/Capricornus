3eec6ba53148cba27d3d1f953acf04b3811f4bb4
satomicchy <satomi_t_sora@yahoo.co.jp>
2014-05-27 19:38:23 +0900

    delete company parameter in companies_controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -64,6 +64,6 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:name, :address, :tel, :fax, :president, :zip, :email, :close_day)
+      params.require(:company).permit(:name, :address, :tel, :fax, :zip, :email, :close_day)
     end
 end
